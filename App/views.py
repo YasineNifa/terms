717b4d563a5f549e9f789a527c3c6929a25f6475
YasineNifa <yasine.nifa@gmail.com>
2023-01-02 20:04:33 +0100

    Fix the contact save method
    Signed-off-by: YasineNifa <yasine.nifa@gmail.com>

--- App/views.py
+++ App/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-
+from .models import Contact
+from django.contrib import messages
 # Create your views here.
 def home(request):
+    if request.method == "POST":
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        terms = request.POST.get('terms')
+        contact.name = name
+        contact.email = email
+        contact.subject = subject
+        contact.message = message
+        contact.terms = terms
+
+        contact.save()
+        messages.success(request, "The message was sent successefully !")
+        context = {
+            "contact": contact,
+            "messages": messages
+        }
+        return render(request,"home.html")
     return render(request,"home.html")
